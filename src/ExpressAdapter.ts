dd108ebc064aad943a42a576c08da9e1ba9ea165
Maycon Oliveira <rick.maycon.98@gmail.com>
2024-06-07 18:07:18 -0300

    cors

--- src/ExpressAdapter.ts
+++ src/ExpressAdapter.ts
@@ -10,6 +10,9 @@ export default class ExpressAdapter implements HttpServer {
 
 	async register(method: string, url: string, callback: Function): Promise<void> {
 		this.app[method](url, async function (req: any, res: any) {
+			res.setHeader( "Access-Control-Allow-Origin", "*" );
+			res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			res.setHeader("Access-Control-Allow-Methods", "GET, POST")
 
 			const output = await callback(req, req.body);
 			res.json(output);
